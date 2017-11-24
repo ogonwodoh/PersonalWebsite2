6cb1fee23072983b3617b41aae4426e10ffb67d0
onwodoh <ocn2000@barnard.edu>
2017-11-23 20:50:36 -0500

    Initial commit

---
+++ php/action_page.php
@@ -0,0 +1,44 @@
+<?php
+ 
+	// grab recaptcha library
+	require_once "recaptchalib.php";
+
+	// your secret key
+	$secret = "6LeeMjoUAAAAAIm8tcDddNTgz2Qd14Hm4SZ6DGMy";
+	
+	// empty response
+	$response = null;
+ 
+	// check secret key
+	$reCaptcha = new ReCaptcha($secret);
+
+	if ($_POST["g-recaptcha-response"]) {
+		$response = $reCaptcha->verifyResponse(
+		$_SERVER["REMOTE_ADDR"],
+		$_POST["g-recaptcha-response"]);
+	}
+	
+	if($response != null && $response->success) {
+		$sender_name=$_POST['fname'];
+		$sender_email=$_POST['femail'];
+		$message=$_POST['fmessage'];
+		$subject = 'www.ogonwodoh.com Message from visitor ' . $sender_name;
+
+		$email_message = "From: " . $sender_name . "\r\nMessage: " . $message ."\r\n";
+		$mail_sent = mail("ogonwodoh@gmail.com", $subject, $email_message);
+
+
+		if ($mail_sent == true){ ?>
+        	<script language="javascript" type="text/javascript">
+        		alert('Thank you for the message. I will contact you shortly.');
+        		window.location = '../html/index.html';
+        	</script>
+    	<?php } else { ?>
+    		<script language="javascript" type="text/javascript">
+        		alert('Message not sent.');
+        		window.location = '../html/index.html';
+    		</script>
+    	<?php
+    	}
+	}
+?>
